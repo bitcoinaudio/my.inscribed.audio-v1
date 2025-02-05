a04b3b1d62179001c7cd29481c86049880e52615
bitcoinaudio <bitcoinaudio@gmail.com>
2025-02-05 17:59:24 -0500

    Update Footer.jsx

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import iaLogo from '/images/ia-bg3.png';
 
 const FooterPage = () => {
   return (
-<div className=" grid grid-col-1 justify-center gap-10 xl:gap-32 py-4">
+<div className=" grid grid-col-2 grid-row-1 wrap justify-center gap-10 xl:gap-16 py-4">
         
 <a href="https://github.com/bitcoinaudio/my.inscribed.audio-v1" className="flex justify-center gap-4">
 <img className="w-12 " src="/images/logo/github.png" alt="gitgub" />
 </a>
+{/* <a href="https://x.com/InscribedAudio" className="flex justify-center gap-4"> 
+<img src={iaLogo} alt="inscribed audio" className="w-12 h-12" />
+</a> */}
+
 <p className="text-center text-sm text-black/70">© 2025 Inscribed Audio</p>
        
 </div>
