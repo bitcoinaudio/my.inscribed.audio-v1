bc98ecaba738516e196fcb2b20b63c2631b5db7a
bitcoinaudio <bitcoinaudio@gmail.com>
2025-02-04 22:02:53 -0500

    Update Footer.jsx

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -4,8 +4,8 @@ const FooterPage = () => {
   return (
 <div className=" grid grid-col-1 justify-center gap-10 xl:gap-32 py-4">
         
-<a href="https://github.com/bitcoinaudio/my.inscribed.audio-v1" >
-<img className="w-12 grayscale" src="/images/logo/github.png" alt="gitgub" />
+<a href="https://github.com/bitcoinaudio/my.inscribed.audio-v1" className="flex justify-center gap-4">
+<img className="w-12 " src="/images/logo/github.png" alt="gitgub" />
 </a>
 <p className="text-center text-sm text-black/70">© 2025 Inscribed Audio</p>
        
